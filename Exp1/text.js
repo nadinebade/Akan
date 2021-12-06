04639a329e51bbc77757af2a3d6b2a0594f8c3df
timeasarvas <91663467+timeasarvas@users.noreply.github.com>
2021-12-06 10:19:14 +0100

    Update text.js
    confidence instead of likelihood

--- Exp1/text.js
+++ Exp1/text.js
@@ -4,14 +4,14 @@ var continue_next2 = new Array;
 
 
 instructions_text.push(
-  "<h1>Instructions (1/2)</h1><p>In this experiment you will hear audio recordings of sentences spoken in Akan. Your task is to judge how likely the sentence was uttered by a native speaker of Akan. You will provide your answer on a sliding scale.</p><p>Before moving on to the examples click 'Next' to read a description of what a trial looks like.</p>");
+  "<h1>Instructions (1/2)</h1><p>In this experiment you will hear audio recordings of sentences spoken in Akan. Your task is to judge how confident you are that the sentence was uttered by a native speaker of Akan. You will provide your answer on a sliding scale.</p><p>Before moving on to the examples click 'Next' to read a description of what a trial looks like.</p>");
 
   instructions_text.push(
-    "<h1>Instructions (2/2)</h1><p>When starting a trial you will hear a sentence spoken in Akan. There will be a 'Replay audio' button which allows you to listen to the sentence again as many times as you wish. The question is how likely it is that a native speaker of Akan uttered the sentence. There will be a slider offering you a range of responses from 'very unlikely' to 'very likely'. You should use it to make your judgment. <strong>You must interact with the slider before you can continue to the next trial. </strong> After each response you will have the chance to leave comments regarding your choice.</p><p>We will now give you <strong>two examples</strong> of the kinds of questions you will be asked.</p>");
+    "<h1>Instructions (2/2)</h1><p>When starting a trial you will hear a sentence spoken in Akan. There will be a 'Replay audio' button which allows you to listen to the sentence again as many times as you wish. The question is how confident you are that a native speaker of Akan uttered the sentence. There will be a slider offering you a range of responses from 'not confident at all' to 'completely confident'. You should use it to make your judgment. <strong>You must interact with the slider before you can continue to the next trial. </strong> After each response you will have the chance to leave comments regarding your choice.</p><p>We will now give you <strong>two examples</strong> of the kinds of questions you will be asked.</p>");
 
 
    continue_next.push(
-   "<p>That was the first example. You should have said that the sentence was very likely uttered by a native speaker of Akan.</p><p>Click 'Next' for another example.</p>");
+   "<p>That was the first example. You should have been completely confident that the sentence was uttered by a native speaker of Akan.</p><p>Click 'Next' for another example.</p>");
 
    continue_next2.push(
-   "<p>That was the second example. You should have said that it is very unlikely that the sentence was uttered by a speaker of Akan.</p><p>Click 'Next' to start the experiment!</p>");
+   "<p>That was the second example. You should have been not confident at all that the sentence was uttered by a speaker of Akan.</p><p>Click 'Next' to start the experiment!</p>");
