0763fdaafd420487f2d7a635c55133ed59bb207f
nadinebade <54040089+nadinebade@users.noreply.github.com>
2022-01-27 11:41:01 +0100

    added condition to items

--- Exp1/materials.js
+++ Exp1/materials.js
@@ -4,36 +4,42 @@ var material = [
     sentence: "A shirt in Benjamin's closet is blue.",
     id: "item.1",
     type: "critical",
+    condition: "pl-match",
   },
 
   { audio: "audio/Audio2.mp3",
   sentence: "A pepper on Abigail's plate is red.",
   id: "item.2",
   type: "critical",
+  condition: "pl-mismatch",
 },
 
 { audio: "audio/Audio3.mp3",
 sentence: "A crayon on Ashley's tray is yellow.",
 id: "item.3",
 type: "critical",
+condition: "sg-match",
 },
 
 { audio: "audio/Audio4.mp3",
 sentence: "A ball in Aaron's box is green.",
 id: "item.4",
 type: "critical",
+condition: "sg-mismatch",
 },
 
 { audio: "audio/Audio5.mp3",
 sentence: "A hoodie in David's suitcase is purple.",
 id: "item.5",
 type: "critical",
+condition: "sg-match",
 },
 
 { audio: "audio/Audio6.mp3",
 sentence: "A baseball cap on Chloe's shelf is orange.",
 id: "item.6",
 type: "critical",
+condition: "sg-match",
 },
  
 
