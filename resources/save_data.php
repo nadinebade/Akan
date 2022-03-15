7b01dd668b0e47aae99cde8678d27fc9f4556fe6
nadinebade <54040089+nadinebade@users.noreply.github.com>
2022-03-15 12:58:50 +0100

    get back to showing data, starting from scratch

--- resources/save_data.php
+++ resources/save_data.php
@@ -2,11 +2,8 @@
 
 // get parameters from call AND escape potential attacks (?)
 $project = escapeshellcmd($_POST['project']);
-$filename = "/homez.48/psycholifz/www/nbade/tmp/exp_" . $project . '/' . $project . "-" . $_POST['filename'] . ".csv";
-$post_data = json_decode(file_get_contents('php://input'), true);
-$data = $post_data['filedata'];
-
-file_put_contents($filename, $data);
+$filename = "/tmp/exp_" . $project . '/' . $project . "-" . $_POST['filename'] . ".csv";
+$data = $_POST['filedata'];
 
 if(!file_exists(dirname($filename)))
     mkdir(dirname($filename), 0777, true);
@@ -20,11 +17,8 @@ if (fwrite($handle, $data) === FALSE)
 fclose($handle);
 chmod ($filename, 0777);
 
-shell_exec("/homez.48/psycholifz/www/nbade/resources/moveFile.sh ".$filename." /homez.48/psycholifz/www/nbade/data/".$project."/ 2>&1");
+shell_exec("/home/smascarenhas/public_html/experiments/resources/moveFile.sh ".$filename." /home/smascarenhas/data/".$project."/ 2>&1");
 
 system("rm -rf ".$filename);
 
 ?>
-
-
-
