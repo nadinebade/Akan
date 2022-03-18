8ace4e387ccd869bf2ed97c20b490f0a7305981a
timeasarvas <91663467+timeasarvas@users.noreply.github.com>
2022-03-18 10:59:45 +0100

    Update text.js
    instruct participants to listen until the end of each file

--- Exp1/text.js
+++ Exp1/text.js
@@ -4,10 +4,10 @@ var continue_next2 = new Array;
 
 
 instructions_text.push(
-  "<h1>Instructions (1/3)</h1><p>In this experiment you will hear audio recordings of sentences spoken in Akan. Your task is to judge how confident you are that the sentence was uttered by a speaker of Akan. You will be asked to provide your answer on a sliding scale.</p><p>Before moving on to the examples click 'Next' to read a description of what a trial looks like.</p>");
+  "<h1>Instructions (1/3)</h1><p>In this experiment you will hear audio recordings of sentences spoken in Akan. Your task is to judge how confident you are that the sentence was uttered by a speaker of Akan. You will be asked to provide your answer on a sliding scale.</p><p>Before moving on to the examples, click 'Next' to read a description of what a trial looks like.</p>");
 
   instructions_text.push(
-    "<h1>Instructions (2/3)</h1><p>To play the audio recording of the Akan sentence press the 'Play audio' button at the beginning of each trial. Then please select how confident you are that the sentence was uttered by a speaker of Akan using the slider. It offers you different options between 'not confident at all' and 'very confident'.</p>");
+    "<h1>Instructions (2/3)</h1><p>To play the audio recording of the Akan sentence, press the 'Play audio' button at the beginning of each trial. Please listen carefully until the end of the audio recording. Then please select how confident you are that the sentence was uttered by a speaker of Akan using the slider. It offers you different options between 'not confident at all' and 'very confident'.</p>");
     
     instructions_text.push(
       "<h1>Instructions (3/3)</h1><p>After each response you will have the chance to leave comments regarding your choice. We will occassionally check your attention by asking content questions regarding the sentences you just heard.</p><p>We will now give you <strong>two examples</strong> of the kinds of questions you will be asked.</p>");
